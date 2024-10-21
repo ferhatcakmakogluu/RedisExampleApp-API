0bb0906bee9b3a858f614ef1de39e7b382ab383c
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2024-10-21 13:36:20 +0300

    AppDbContext, Model and Seed comleted

---
+++ RedisExampleApp.Api/Model/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RedisExampleApp.Api.Model
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Products> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Products>().HasData(
+                new Products { Id=1,Name="Kalem",Price=100},
+                new Products { Id=2, Name="Kitap", Price=200},
+                new Products { Id=3, Name="Defter", Price=50}
+            );
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
